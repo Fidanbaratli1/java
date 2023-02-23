dcc4dbcb815c9987acde2bcb4eaeaa40bee522db
Orxan002 <7ltdajp@code.edu.az>
2023-02-23 12:43:05 +0400

    created file

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int x = 5;
+int y = 10;
+Console.WriteLine(x + y);
+Console.WriteLine(x += y);
+Console.WriteLine(x -= y);
+Console.WriteLine(x *= y);
+int nuray = 5;
+double myName = 7.2D;
+char myLast = 'A';
+bool myBolt = false;
+string myTail = "Hi";
+int a = 5;
+int b = 4;
+Console.WriteLine(a/= b);
+Console.WriteLine(a %=b);
+Console.WriteLine(a &= b);
+Console.WriteLine(a |= b);
+Console.WriteLine(a ^= b);
+Console.WriteLine(a>>=b);
+Console.WriteLine(a<<=b);
+Console.WriteLine(a==b);
+Console.WriteLine(a!=b);
+Console.WriteLine(a>b);
+Console.WriteLine(a<b);
+Console.WriteLine(a>=b);
+Console.WriteLine(a<=b);
---
+++ time/main.js
@@ -0,0 +1,33 @@
+let a = 5
+console.log(a);
+let  b = 9
+console.log(a+b);
+console.log(a-b);
+console.log(a*b)
+console.log(a**b)
+console.log(a/b)
+console.log(a%b)
+console.log(++a)
+console.log(--a)
+console.log(a=b)
+console.log(a+=b)
+console.log(a-=b)
+console.log(a*=b)
+console.log(a/=b)
+console.log(a%=b)
+console.log(a**=b)
+console.log(a=b)
+let x = "false"
+let y = 2
+console.log(x+y)
+let c=6
+let d=2
+console.log(c=d);
+console.log(c==d)
+console.log(c===d)
+console.log(c!=d)
+console.log(c!==d)
+console.log(c>d)
+console.log(c<d)
+console.log(c&&d)
+console.log(c||d)
